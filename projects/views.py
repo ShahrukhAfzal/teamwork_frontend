2c1e972ee8350db231c5b30332fbb4b2c83c140d
shahrukh <shoaibtayyab121@gmail.com>
2021-02-14 16:57:42 +0530

    working on creating projects

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,31 @@
+import requests
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
-# Create your views here.
+from teamwork_frontend.config import ROOT_API_URL
 
-def index(request):
-    context = {'message': 'success'}
-    return render(request, 'projects/index.html', context=context)
+
+def ListProjects(request):
+    r = requests.get(ROOT_API_URL + 'project-app/projects/')
+    project_list = r.json()
+    context = {'message': 'success', 'projects': project_list}
+    return render(request, 'projects/list.html', context=context)
+
+
+def CreateProject(request):
+    if request.method == 'GET':
+        return render(request, 'projects/create.html')
+    else:
+        name = request.POST['name']
+        description = request.POST['description']
+
+        context = {
+            'name': name,
+            'description': description
+        }
+
+        r = requests.post(ROOT_API_URL + 'project-app/projects/', data=context)
+
+        return redirect('list-project')
